8703d8e033c3f2bf97479f14844e87dc8225c697
Anas Bajjouk <bajjouk.a@office.lan>
2020-06-22 16:50:47 +0200

    connecting the store to react

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+    message: `It's easy to integrate React and Redux :p`,
+}
+
+const reducer = (state = INITIAL_STATE) => {
+
+    return state
+}
+
+export default reducer
